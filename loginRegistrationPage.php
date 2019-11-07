90acf2912c879646bb980804fded75edead88d73
RagRoyale <rr543@njit.edu>
2019-11-07 17:19:59 -0500

    Fixes

--- loginRegistrationPage.php
+++ loginRegistrationPage.php
@@ -4,28 +4,32 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //pull in config.php so we can access the variables from it
-require('config.php');
-echo "Loaded Host: " . $host;
-$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-try{
+
+if(isset($_POST["username"]) && isset($_POST["password"]){
+
+   require('config.php');
+   echo "Loaded Host: " . $host;
+   $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+   try{
 	$db = new PDO($conn_string, $username, $password);
         echo "Connected";
 	
 	
 	
-	//TODO select query using bindable :username is where clause
-	//select * from TestUsers where username =
-	$select_query = "select * from `Login` where username = :username";
-	$stmt = $db->prepare($select_query);
-	$r = $stmt->execute(array(":username"=> "RagRoyale"));
-	$results = $stmt->fetch(PDO::FETCH_ASSOC);
-	//print_r($stmt->errorInfo());
-	echo "<pre>" . var_export($results, true) . "</pre>"; 
-}
-catch(Exception $e){
-	echo $e->getMessage();
-	exit("Something went wrong");
-}
+         	//TODO select query using bindable :username is where clause
+	       //select * from TestUsers where username =
+  	       $select_query = "select * from `Login` where username = :username";
+	       $stmt = $db->prepare($select_query);
+	       $r = $stmt->execute(array(":username"=> "RagRoyale"));
+	       $results = $stmt->fetch(PDO::FETCH_ASSOC);
+	       //print_r($stmt->errorInfo());
+	       echo "<pre>" . var_export($results, true) . "</pre>"; 
+   }
+   catch(Exception $e){
+	 echo $e->getMessage();
+	 exit("Something went wrong");
+   }
+  }
 ?>
 
 <html lang="en">
